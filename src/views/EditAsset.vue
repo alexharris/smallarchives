48b8e5a67b8f270544f498b8963c93f66a414504
alexharris <alexharris6@gmail.com>
2019-01-14 18:51:40 -0800

    ability to remove custom fields from editasset page

--- src/views/EditAsset.vue
+++ src/views/EditAsset.vue
@@ -3,6 +3,8 @@
     <b-col cols="12">
       <b-btn @click.stop="goBackOne">Back</b-btn>
       <hr class="my-4" />
+      <h1>{{asset.assetTitle}}</h1>      
+      <hr class="my-4" />
       {{asset.assetName}}
       <b-form @submit="onSubmit">
         <b-form-group id="fieldsetHorizontal"
@@ -26,9 +28,25 @@
             <td>{{item.fieldValue}}</td>
             <td><b-btn @click.stop="deleteCustomField(itemId)">Delete</b-btn></td>
           </tr>
-        </table>        
-
-        <b-button type="submit" variant="primary">Update</b-button>
+        </table> 
+        <div>
+          <b-btn v-b-toggle.collapse1 variant="primary">Add Custom Field</b-btn>
+          <b-collapse id="collapse1" class="mt-2">
+            <b-card>
+              <b-form-group label="Label"
+                            label-for="customFieldLabel">
+                <b-form-input id="customFieldLabel" v-model="newCustomField.fieldLabel"></b-form-input>
+              </b-form-group>
+              <b-form-group label="Value"
+                            label-for="customFieldValue">
+                <b-form-input id="customFieldValue" v-model="newCustomField.fieldValue"></b-form-input>
+              </b-form-group>
+              <b-btn variant="primary" @click.stop="addCustomField()">Add</b-btn>
+            </b-card>
+          </b-collapse>
+        </div>               
+        <hr class="my-4" />            
+        <b-button type="submit" variant="primary">Save</b-button>
         <hr my="4" />
         <b-alert show variant="danger">
           <h4>Delete</h4>
@@ -55,6 +73,10 @@ export default {
         assetId: '',
         customFields: ''
       },
+      newCustomField: {
+        fieldLabel:'',
+        fieldValue:''
+      }
     }
   },
   created () {
@@ -84,7 +106,8 @@ export default {
       const ref = firebase.firestore().collection('archives').doc(firebase.auth().currentUser.uid).collection('userarchives').doc(this.$route.params.archive_id).collection('assets').doc(this.$route.params.asset_id);
 
       ref.update({
-        assetTitle: this.asset.assetTitle
+        assetTitle: this.asset.assetTitle,
+        customFields: this.asset.customFields
 
       }).then(() => {
         console.log('asset updated!')
@@ -93,23 +116,23 @@ export default {
     },
     deleteCustomField(assetId) {
 
-      var customFields;
+      // var customFields;
 
-      const ref = firebase.firestore().collection('archives').doc(firebase.auth().currentUser.uid).collection('userarchives').doc(this.$route.params.archive_id).collection('assets').doc(this.$route.params.asset_id);
-      // get the fields from the database and assign to asset array
-      ref.get().then((doc) => {
-        if (doc.exists) {
-          customFields = doc.data().customFields
-          customFields.splice(assetId, 1)
-        } else {
-          console.log("No such document!");
-        }
-      }).then(() => {
-        this.asset.customFields = customFields
-        ref.update({
-          customFields: customFields
-        })
-      }); 
+      // const ref = firebase.firestore().collection('archives').doc(firebase.auth().currentUser.uid).collection('userarchives').doc(this.$route.params.archive_id).collection('assets').doc(this.$route.params.asset_id);
+      // // get the fields from the database and assign to asset array
+      // ref.get().then((doc) => {
+      //   if (doc.exists) {
+      //     customFields = doc.data().customFields
+      this.asset.customFields.splice(assetId, 1)
+      //   } else {
+      //     console.log("No such document!");
+      //   }
+      // }).then(() => {
+      //   this.asset.customFields = customFields
+      //   ref.update({
+      //     customFields: customFields
+      //   })
+      // }); 
 
     },
     itemDelete(assetId) {
@@ -134,6 +157,11 @@ export default {
 
 
     },
+    addCustomField () {
+      this.asset.customFields.push({fieldLabel: this.newCustomField.fieldLabel, fieldValue: this.newCustomField.fieldValue });
+      this.newCustomField.fieldLabel = ''
+      this.newCustomField.fieldValue = ''
+    },    
     goBackOne() {
       this.$router.go(-1)
     },         
