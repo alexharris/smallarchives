dddc576cf659bfe0a1610c3f40eef775405f2671
alexharris <alexharris6@gmail.com>
2019-02-10 23:38:39 -0800

    remove usernav compoenent

--- src/components/UserNav.vue
+++
@@ -1,8 +0,0 @@
-<template>
- 	<b-nav pills>
-	    <b-nav-item>
-	        <router-link class="nav-link" active-class="active" to="/admin/add-archive">Add +</router-link>
-	    </b-nav-item>	    
- 	</b-nav> 
-</template>
-
